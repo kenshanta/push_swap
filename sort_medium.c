62a44061db648ff78c0ad17df24b013e3d387f3c
Sarkis Kovlekjian <skovlekj@c3r7s3.42warsaw.pl>
2026-09-13 18:04:34 +0200

    create file

---
+++ sort_medium.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_medium.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skovlekj <skovlekj@student.42warsaw.pl>    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-09-11 19:32:15 by skovlekj          #+#    #+#             */
+/*   Updated: 2026-09-11 19:32:15 by skovlekj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "push_swap.h"
+
+void    sort_medium(t_stacks *toolbox)
+{
+    int stack_len;
+
+    if (toolbox->a)
+        stack_len = stack_length(toolbox->a);
+}
