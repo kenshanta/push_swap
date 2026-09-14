f035345818e7f34cf7bee48c669d6cc8c99e9ee2
Sarkis Kovlekjian <skovlekj@c3r8s4.42warsaw.pl>
2026-09-14 17:06:10 +0200

    update header file with medium algo inclusion

--- push_swap.h
+++ push_swap.h
@@ -50,6 +50,7 @@ typedef struct s_stacks
 //strategies
 void	push_swap(t_stacks **toolbox);
 void	simple_sort (t_stacks *toolbox);
+void    medium_sort(t_stacks *toolbox);
 void	assign_indexes(t_stacks **toolbox);
 //stack maintenance
 t_stacks	*stacks_init(void);
